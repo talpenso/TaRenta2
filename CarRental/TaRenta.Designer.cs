8733407be25e31a994157da3cd07012559d18e23
talp <talpenso8@gmail.com>
2020-01-28 15:28:29 +0200

    env set to tal laptop

--- CarRental/TaRenta.Designer.cs
+++ CarRental/TaRenta.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model '\\files\users_data$\talp\My Documents\jhonBryce\FinalProject\CarRental\CarRental\TaRenta.edmx'. 
+﻿// T4 code generation is enabled for model '\\files\users_data$\talp\My Documents\jhonBryce\FinalProject\TaRenta2\CarRental\TaRenta.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
