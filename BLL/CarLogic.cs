542e8fbd0101120416c03ed0b8d3c4b2c7a6b5b5
ran <ran@ncr.co.il>
2020-01-29 14:21:41 +0200

    Added Car Logic

--- BLL/CarLogic.cs
+++ BLL/CarLogic.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarRental 
+namespace CarRental
 {
     class CarLogic : BaseLogic
     {
@@ -14,15 +14,15 @@ namespace CarRental
         public DTOCar ADDCar(DTOCar DTOCar)
         {
 
-        //public int CarNumber { get; set; }
-        //public int CarTypeID { get; set; }
-        //public int CurrentKilometrage { get; set; }
-        //public string CarPicture { get; set; }
-        //public bool IsServiceable { get; set; }
-        //public bool IsAvailable { get; set; }
-        //public int BranchID { get; set; }
+            //public int CarNumber { get; set; }
+            //public int CarTypeID { get; set; }
+            //public int CurrentKilometrage { get; set; }
+            //public string CarPicture { get; set; }
+            //public bool IsServiceable { get; set; }
+            //public bool IsAvailable { get; set; }
+            //public int BranchID { get; set; }
 
-        Car dbCar = new Car();
+            Car dbCar = new Car();
 
             dbCar.CarNumber = DTOCar.CarNumber;
             dbCar.CarTypeID = DTOCar.CarTypeId;
@@ -34,6 +34,56 @@ namespace CarRental
 
             dbCar = db.Cars.Add(dbCar);
             db.SaveChanges();
+
+            return DTOCar;
+        }
+
+
+        public List<DTOCar> GetCars()
+        {
+            return db.Cars
+                .Select(s => new DTOCar()
+                {
+
+                    CarNumber = s.CarNumber,
+                    CarTypeId = s.CarTypeID,
+                    CurrentKilometers = s.CurrentKilometrage,
+                    CarPicture = s.CarPicture,
+                    IsServiceable = s.IsServiceable,
+                    IsAvailable = s.IsAvailable,
+                    BranchId = s.BranchID
+
+                }).ToList();
+        }
+
+        public void UpdateCar(DTOCar DTOCar)
+        {
+            Car DALCar = db.Cars.Find(DTOCar.CarNumber);
+
+            DALCar.CarNumber = DTOCar.CarNumber;
+            DALCar.CarTypeID = DTOCar.CarTypeId;
+            DALCar.CurrentKilometrage = DTOCar.CurrentKilometers;
+            DALCar.CarPicture = DTOCar.CarPicture;
+            DALCar.IsServiceable = DTOCar.IsServiceable;
+            DALCar.IsAvailable = DTOCar.IsAvailable;
+            DALCar.BranchID = DTOCar.BranchId;
+
+            db.SaveChanges();
+        }
+
+        public DTOCar GetCar(int CarNumber)
+        {
+            Car DALCar = db.Cars.Find(CarNumber);
+
+            DTOCar DTOCar = new DTOCar();
+
+            DTOCar.CarNumber = DALCar.CarNumber;
+            DTOCar.CarTypeId = DALCar.CarTypeID;
+            DTOCar.CurrentKilometers = DALCar.CurrentKilometrage;
+            DTOCar.CarPicture = DALCar.CarPicture;
+            DTOCar.IsServiceable = DALCar.IsServiceable;
+            DTOCar.IsAvailable = DALCar.IsAvailable;
+            DTOCar.BranchId = DALCar.BranchID;
 
             return DTOCar;
         }
